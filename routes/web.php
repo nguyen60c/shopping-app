5526cc643bf3610e823f2812d125433a96100bd8
nguyenlam <nguyen60c@gmail.com>
2022-04-16 09:15:30 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(["namspace"=>"App\Http\Controllers"],function(){
+
+    /*
+     * Home Routes
+     */
+    Route::get("/","HomeController@index")->name("home.index");
+
+    Route::middleware("guest")->group(function(){
+        /*
+         * Register Routes
+         */
+
+        /*Display form*/
+        Route::get("/register","auth\RegisterController@show")
+            ->name("register.show");
+        /*Create new account*/
+        Route::post("/register","auth\RegisterController@register")
+            ->name("register.perform");
+
+        /*
+         * Login Routes
+         */
+
+        /*Display login form*/
+        Route::get("login","auth\LoginController@show")
+            ->name("login.show");
+
+        /*Login user*/
+        Route::post("login","auth\LoginController@login")
+            ->name("login.perform");
+    });
+
+    Route::middleware("auth")->group(function(){
+        /*
+         * Logout Routes
+         */
+        Route::get("logout","users\LogoutController@perform")
+            ->name("logout.perform");
+    });
 });
